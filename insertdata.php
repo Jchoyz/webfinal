da1e3718ca89aa9fcfc2434317cdbfca3410bce1
Jim Choy <choyjim33@gmail.com>
2024-12-17 12:10:53 -0500

    Add files via upload

---
+++ insertdata.php
@@ -0,0 +1,43 @@
+<?php
+    $servername = "localhost";
+    $username = "chickenjr";
+    $password = "voidnul0";
+    $dbname = "final";
+
+    // Create connection
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        //$name = htmlspecialchars($_POST['firstname']) . " " . htmlspecialchars($_POST['lastname']);
+        $search = htmlspecialchars($_POST['searchterm']);
+        $ip = htmlspecialchars($_SERVER['REMOTE_ADDR']);
+
+        // Insert data into the database
+        $sql = "INSERT INTO HackingUsers (searchterm, IPaddr)
+        VALUES (?, ?);";
+
+        if (mysqli_query($conn, $sql)) {
+            echo "New record created successfully";
+        } 
+        else {
+            echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+        }
+    }
+
+
+    if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+
+    header("Location: https://www.google.com/search?q="  .  urlencode($search));
+    exit();
+?>
